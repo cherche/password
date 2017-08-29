284dac70c17d0ea61076cd8340b3ff1fe4a5188a
Loquacious <ryannguyen627939@gmail.com>
2017-08-28 22:25:25 -0400

    Create generate function

--- password.js
+++ password.js
@@ -1,9 +1,24 @@
 const LATIN_ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 
-const toArray = str => str.split('')
-const shuffle = (arr) => {
-  if (arr.length === 1) return arr
+/* Experimental function chaining
+const chain = (thing, fns) => {
+  let copy = thing
 
+  if (Array.isArray(thing)) copy.slice()
+
+  for (let i = 0; i < fns.length; i++) {
+    copy = fns[i](copy)
+  }
+
+  return copy
+}
+*/
+
+const intoArray = str => str.split('')
+const intoString = arr => arr.join('')
+
+const getRandomEl = arr => arr[Math.floor(Math.random() * arr.length)]
+const shuffle = (arr) => {
   // Create copy to avoid mutation
   const copy = arr.slice()
   let i = copy.length
@@ -18,6 +33,7 @@ const shuffle = (arr) => {
 
   return copy
 }
+const trim = str => str.replace(/^\s+|\s+$/g, '')
 
 const sets = {
   uppercase: LATIN_ALPHABET.toUpperCase(),
@@ -29,12 +45,12 @@ const sets = {
 
 // Transform all strings into arrays
 for (const key of Object.keys(sets)) {
-  sets[key] = toArray(sets[key])
+  sets[key] = intoArray(sets[key])
 }
 
 const def = {
   sets,
-  length: 15,
+  length: 16,
   uppercase: true,
   lowercase: true,
   numbers: true,
@@ -42,8 +58,29 @@ const def = {
   spaces: true
 }
 
-const generate = (custom) => {
+const generate = (custom = {}) => {
   // Use Object.assign() to overwrite any default values
   // {} is passed in to avoid mutation
   const config = Object.assign({}, def, custom)
+  config.sets = Object.assign({}, sets, custom.sets || {})
+
+  let fullSet = []
+  const password = []
+
+  // Build collection of all sets
+  for (const key of Object.keys(config.sets)) {
+    if (config[key]) {
+      /* Guarantee that the password contains
+      // at least one character from each set
+      password.push(getRandomEl(config.sets[key]))
+      */
+      fullSet = fullSet.concat(config.sets[key])
+    }
+  }
+
+  // Select random character from total set each time and add
+  while (password.length < config.length) password.push(getRandomEl(fullSet))
+
+  // Human-readable and usable format
+  return intoString(password)
 }
